906ef803ed7c3b64075410f22971b8c1e512f95d
edwinmgallego <ing.edwin.gallego@gmail.com>
2024-11-09 16:55:12 -0500

    implementacion de materialize a fuego!

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
-const btn = document.getElementById('button');
+const btn = document.getElementById("button");
 
-document.getElementById('form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
+document.getElementById("form").addEventListener("submit", function (event) {
+  event.preventDefault();
 
-   btn.value = 'Sending...';
+  btn.value = "Sending...";
 
-   const serviceID = 'default_service';
-   const templateID = 'template_p0nz6cf';
+  const serviceID = "default_service";
+  const templateID = "template_p0nz6cf";
 
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      btn.value = 'Send Email';
-      alert('Sent!');
-    }, (err) => {
-      btn.value = 'Send Email';
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      btn.value = "Send Email";
+      alert("Sent!");
+    },
+    (err) => {
+      btn.value = "Send Email";
       alert(JSON.stringify(err));
-    });
+    }
+  );
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".materialboxed");
+  var instances = M.Materialbox.init(elems, options);
 });
